ac1c508dfd2774acb64cfb4c90bba815979a4fe6
earltacs <syntacs.2019@gmail.com>
2022-01-24 00:12:13 +0800

    UPDATE index
    - Made a sample update to push for vercel preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="block h-screen w-screen">
+    <div class="flex flex-col items-center">
+        <h1 class="text-6xl text-orange-600 uppercase">Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev" class="text-blue-400 underline">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</main>
